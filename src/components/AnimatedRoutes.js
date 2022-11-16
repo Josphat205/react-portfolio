a00cab06a6e739cfde32ad9318f1f9d575ec5fb9
lomantech <87063237+lomantech@users.noreply.github.com>
2022-11-16 14:53:27 +0300

    Add frame-motion

---
+++ src/components/AnimatedRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Skills from "./Skills";
+import Testimonial from "./Testimonial";
+import Work from "./Work";
+import { Route, Routes, useLocation } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import { AnimatePresence } from "framer-motion";
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <div>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/testimonial" element={<Testimonial />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default AnimatedRoutes;
